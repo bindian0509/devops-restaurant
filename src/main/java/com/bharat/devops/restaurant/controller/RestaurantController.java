7bf32d883ad41a52bf9d3bd5ec130fdea3dbc6a4
Bharat V <bindian0509@gmail.com>
2023-10-02 23:59:59 +0530

    Added Restaurant Controller

--- src/main/java/com/bharat/devops/restaurant/controller/RestaurantController.java
+++ src/main/java/com/bharat/devops/restaurant/controller/RestaurantController.java
@@ -4,5 +4,37 @@ package com.bharat.devops.restaurant.controller;
  * @author Bharat V. <bindian0509@gmail.com>
  * @created Thursday, 11 January 2024
  */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/restaurant")
+@CrossOrigin
 public class RestaurantController {
-}
+
+    @Autowired
+    RestaurantService restaurantService;
+
+
+
+    @GetMapping("/fetchAllRestaurants")
+    public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
+        List<RestaurantDTO> allRestaurants = restaurantService.findAllRestaurants();
+        return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
+    }
+
+    @PostMapping("/addRestaurant")
+    public ResponseEntity<RestaurantDTO> saveRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
+        RestaurantDTO restaurantAdded = restaurantService.addRestaurantInDB(restaurantDTO);
+        return new ResponseEntity<>(restaurantAdded, HttpStatus.CREATED);
+    }
+
+    @GetMapping("fetchById/{id}")
+    public ResponseEntity<RestaurantDTO> findRestaurantById(@PathVariable Integer id) {
+        return restaurantService.fetchRestaurantById(id);
+    }
+
+
