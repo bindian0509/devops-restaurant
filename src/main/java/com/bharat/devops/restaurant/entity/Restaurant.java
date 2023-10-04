e8addc237ffb9511920b5218e924b69a82b4e236
Bharat V <bindian0509@gmail.com>
2023-10-04 23:59:59 +0530

    Added Restaurant Entity

---
+++ src/main/java/com/bharat/devops/restaurant/entity/Restaurant.java
@@ -0,0 +1,32 @@
+package com.bharat.devops.restaurant.entity;
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Thursday, 11 January 2024
+ */
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String address;
+    private String city;
+    private String restaurantDescription;
+
+}
+
