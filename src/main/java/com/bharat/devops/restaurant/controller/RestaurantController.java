ac89a3fbaef6e20414914609b301925cda79335d
Bharat V <bindian0509@gmail.com>
2024-01-11 19:49:27 +0530

    first commit

---
+++ src/main/java/com/bharat/devops/restaurant/controller/RestaurantController.java
@@ -0,0 +1,8 @@
+package com.bharat.devops.restaurant.controller;
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Thursday, 11 January 2024
+ */
+public class RestaurantController {
+}
